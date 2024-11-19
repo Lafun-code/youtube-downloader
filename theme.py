c1e308257dd4b4b28ef6a6855efa6fe6efce2ee9
Lafun-code <68625577+Lafun-code@users.noreply.github.com>
2024-11-19 22:49:18 +0300

    Add files via upload

---
+++ theme.py
@@ -0,0 +1,39 @@
+theme_stylesheet = """
+    QWidget {
+        background-color: #f2f2f2;
+        font-family: 'Arial', sans-serif;
+        color: #333333;
+    }
+    QLabel {
+        font-size: 14px;
+        color: #444;
+    }
+    QLineEdit {
+        padding: 5px;
+        border: 1px solid #cccccc;
+        border-radius: 5px;
+        background-color: #ffffff;
+        font-size: 14px;
+    }
+    QRadioButton {
+        font-size: 14px;
+        padding-left: 10px;
+    }
+    QPushButton {
+        background-color: #5cb85c;
+        color: white;
+        padding: 10px;
+        font-size: 16px;
+        border-radius: 8px;
+        border: none;
+    }
+    QPushButton:hover {
+        background-color: #4cae4c;
+    }
+    QPushButton:pressed {
+        background-color: #46a046;
+    }
+    QLineEdit:focus {
+        border: 1px solid #5cb85c;
+    }
+"""
